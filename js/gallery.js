aea25d311408eadee326c9a8c77b0686a7aad432
bhav-web <bhav-web@github.com>
2021-02-25 22:21:41 +0530

    javascript

--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,22 @@
-function upDate(previewPic){
+function upDate(previewPic)
+{
     /* In this function you should 
        1) change the url for the background image of the div with the id = "image" 
        to the source file of the preview image
        
        2) Change the text  of the div with the id = "image" 
-       to the alt text of the preview image 
+       to the alt text of the preview image
+       
        */
+    console.log(previewPic.getAttribute('src'))
+    // var image= document.getElememtById('image')
+    image.style.backgroundImage= "url('"+ previewPic.getAttribute('src') +"')"
+    image.innerHTML = previewPic.getAttribute('alt')
+    console.log(image.innerHTML)
      
-       }
-   
-       function unDo(){
+ }
+ function unDo()
+{
         /* In this function you should 
        1) Update the url for the background image of the div with the id = "image" 
        back to the orginal-image.  You can use the css code to see what that original URL was
@@ -17,5 +24,7 @@ function upDate(previewPic){
        2) Change the text  of the div with the id = "image" 
        back to the original text.  You can use the html code to see what that original text was
        */
+    image.style.backgroundImage= ""
+    image.innerHTML = "Hover over an image below to display here."
            
        }
